c02e4f09a4a81e51acca57960af22e408da3fca4
Jonathan Walter <jonathan@twotype.se>
2016-10-18 23:22:45 +0200

    Adding initial dotfiles for Hammerspoon

---
+++ hammerspoon/windowfocus.lua
@@ -0,0 +1,45 @@
+local hyper = {"shift", "cmd", "alt", "ctrl"}
+
+-----------------------------------------------
+-- Hyper i to show window hints
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'i', function()
+    hs.hints.windowHints()
+end)
+
+-----------------------------------------------
+-- Hyper arrows to switch window focus
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'Up', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowNorth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'Down', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowSouth()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'Right', function()
+    if hs.window.focusedWindow() then
+    hs.window.focusedWindow():focusWindowEast()
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+hs.hotkey.bind(hyper, 'Left', function()
+    if hs.window.focusedWindow() then
+        hs.window.focusedWindow():focusWindowWest()
+    else
+        hs.alert.show("No active window")
+    end
+end)
