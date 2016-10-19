437da6622d35aaade20df05acb5fffaf5c3f9db0
Jonathan Walter <jonathan@twotype.se>
2016-10-19 22:31:23 +0200

    Added hammerspoon

--- make-links.sh
+++ make-links.sh
@@ -1,7 +1,7 @@
 DIR=$(dirname $(realpath $0)) #$(dirname $0)
 DATE=$(date "+%Y-%m-%d-%H.%M")
 
-
+################# zshrc
 if [[ -f ~/.zshrc && ! -h ~/.zshrc ]]; then
 	echo Backing up old .zshrc to ~/.zshrc.$DATE 
 	mv ~/.zshrc ~/.zshrc.$DATE
@@ -14,7 +14,7 @@ echo "Linking .zshrc to $(dirname $(realpath $0))/zsh/zshrc"
 ln -s $DIR/zsh/zshrc ~/.zshrc
 
 
-
+################# zgen
 if [[ -d ~/.zgen && ! -h ~/.zgen ]]; then
 	echo Backing up old .zgen to ~/.zgen.$DATE 
 	mv ~/.zgen ~/.zgen.$DATE
@@ -25,3 +25,16 @@ fi
 
 echo "Linking .zgen to $(dirname $(realpath $0))/zsh/zgen"
 ln -s $DIR/zsh/zgen ~/.zgen
+
+
+################# hammerspoon
+if [[ -d ~/.hammerspoon && ! -h ~/.hammerspoon ]]; then
+  echo Backing up old .hammerspoon to ~/.hammerspoon.$DATE 
+  mv ~/.hammerspoon ~/.hammerspoon.$DATE
+elif [[ -h ~/.hammerspoon ]]; then
+  echo "Unlinking old link"
+  unlink ~/.hammerspoon
+fi
+
+echo "Linking .hammerspoon to $(dirname $(realpath $0))/hammerspoon"
+ln -s $DIR/zsh/hammerspoon ~/.hammerspoon
