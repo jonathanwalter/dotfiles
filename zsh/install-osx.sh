1b90e39f28322e3cd277beed81e2956441eeb48c
Jonathan Walter <jonathan@twotype.se>
2017-01-06 11:03:24 +0100

    Renamed for osx

--- zsh/install-osx.sh
+++ zsh/install-osx.sh
@@ -4,4 +4,4 @@ git clone https://github.com/tarjoilija/zgen.git $(dirname $(realpath $0))/zgen
 
 
 # Change to the Homebrew version of zsh
-#sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells" && chsh -s /usr/local/bin/zsh
+sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells" && chsh -s /usr/local/bin/zsh
