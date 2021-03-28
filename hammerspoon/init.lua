fd51434caab81f222cad3ed025e7d9ab24150c93
Jonathan Walter <jonathan@twotype.se>
2021-03-28 22:31:53 +0200

    Add stackline

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,13 +1,31 @@
-hyper = {"ctrl", "alt", "cmd"}
-hypershift = {"ctrl", "alt", "cmd", "shift"}
-
 -- Reload config on write
 function reload_config(files)
     hs.reload()
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config loaded")
+hs.alert.show("Config reloaded")
 
 -- Custom stuff
 -- require "windowborder"
-require('position')
+-- require('position')
+
+stackline = require "stackline.stackline.stackline"
+
+local stacklineConfig = {
+    appearance = {
+        showIcons = true,
+        radius = 6,
+        dimmer = 4,
+        iconDimmer = 1,
+        iconPadding = 4,
+        size = 30,
+        pillThinness = 4,
+    },
+    features = {
+        fzyFrameDetect = {
+            fuzzFactor = 20
+        },
+    },
+}
+
+stackline:init(stacklineConfig)
