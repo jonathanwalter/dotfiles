ff7fa2189900c43a29da3818ca509df4b68c0474
Jonathan Walter <jonathan@twotype.se>
2021-03-30 20:14:18 +0200

    styling update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,10 @@
 -- Reload config on write
 function reload_config(files)
     hs.reload()
+    hs.alert.show("Config reloaded")
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config reloaded")
+hs.alert.show("Config loaded")
 
 -- Custom stuff
 -- require "windowborder"
@@ -13,10 +14,11 @@ stackline = require "stackline.stackline.stackline"
 
 local stacklineConfig = {
     appearance = {
+        color = { white = 0 },
         showIcons = true,
         radius = 6,
-        dimmer = 4,
-        iconDimmer = 1,
+        dimmer = 5,
+        iconDimmer = 1.5,
         iconPadding = 4,
         size = 30,
         pillThinness = 4,
