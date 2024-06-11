0311a7eb5634f930767632182cd15e5b6614521d
Jonathan Walter <jonathan@walter.pink>
2024-06-11 16:43:49 +0200

    feat: improve tab titles

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,11 @@ local act = wezterm.action
 
 local config = {}
 
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+
 config.front_end = "WebGpu"
 
 config.window_decorations = "RESIZE"
@@ -14,9 +19,9 @@ config.window_padding = {
 }
 
 config.font = wezterm.font_with_fallback {
-  "JetBrainsMono Nerd Font",
-  "SFMono Nerd Font",
-  "MesloLGS NF",
+  -- "MesloLGL Nerd Font",
+  -- "JetBrainsMono Nerd Font",
+  -- "SFMono Nerd Font",
 }
 
 config.font_size = 16.0
@@ -106,6 +111,9 @@ config.keys = {
 
 }
 
+config.status_update_interval = 1000
+
+
 function current_hostname(pane)
     local procinfo = pane:get_foreground_process_info()
     local name = pane:get_title()
@@ -125,14 +133,13 @@ function current_hostname(pane)
             hostname = "lima[" .. hostname .. "]"
         -- assume localhost
         else
-            hostname = 'localhost'
+            hostname = ''
         end
     end
 
     return hostname
 end
 
-config.status_update_interval = 1000
 
 wezterm.on('update-right-status', function(window, pane)
     local status_hostname = current_hostname(pane)
@@ -159,6 +166,42 @@ end)
 
 
 
+local process_icons = {
+  ['docker'] = wezterm.nerdfonts.linux_docker,
+  ['docker-compose'] = wezterm.nerdfonts.linux_docker,
+  ['psql'] = wezterm.nerdfonts.dev_postgresql,
+  ['kubectl'] = wezterm.nerdfonts.linux_docker,
+  ['nvim'] = wezterm.nerdfonts.custom_vim,
+  ['make'] = wezterm.nerdfonts.seti_makefile,
+  ['vim'] = wezterm.nerdfonts.dev_vim,
+  ['go'] = wezterm.nerdfonts.seti_go,
+  ['zsh'] = wezterm.nerdfonts.cod_terminal,
+  ['bash'] = wezterm.nerdfonts.cod_terminal_bash,
+  ['htop'] = wezterm.nerdfonts.mdi_chart_donut_variant,
+  ['cargo'] = wezterm.nerdfonts.dev_rust,
+  ['sudo'] = wezterm.nerdfonts.fa_hashtag,
+  ['lazydocker'] = wezterm.nerdfonts.linux_docker,
+  ['git'] = wezterm.nerdfonts.dev_git,
+  ['lua'] = wezterm.nerdfonts.seti_lua,
+  ['python'] = wezterm.nerdfonts.dev_python,
+  ['wget'] = wezterm.nerdfonts.mdi_arrow_down_box,
+  ['curl'] = wezterm.nerdfonts.mdi_flattr,
+  ['ruby'] = wezterm.nerdfonts.cod_ruby,
+  ['node'] = wezterm.nerdfonts.dev_nodejs_small,
+  ['ssh'] = wezterm.nerdfonts.md_remote_desktop,
+}
+
+
+
+
+local function get_process_icon(tab)
+  local process_name = string.lower(tab.active_pane.foreground_process_name:match("([^/\\]+)$"))
+  local icon = process_icons[process_name] or wezterm.nerdfonts.seti_checkbox_unchecked
+
+  return icon
+end
+
+
 function tab_title(tab_info)
   local title = tab_info.tab_title
   if title and #title > 0 then -- explicitly set
@@ -167,20 +210,35 @@ function tab_title(tab_info)
   return tab_info.active_pane.title --title from the active pane
 end
 
-wezterm.on(
-  'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
-    local title = tab_title(tab)
 
-    return {
-      -- { Background = { Color = 'blue' } },
-      { Text = ' ' .. tab.tab_index .. ': ' .. title .. ' ' },
-    }
 
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local has_unseen_output = false
+  local is_zoomed = false
+
+  for _, pane in ipairs(tab.panes) do
+    if not tab.is_active and pane.has_unseen_output then
+      has_unseen_output = true
+    end
+    if pane.is_zoomed then
+      is_zoomed = true
+    end
   end
-)
 
+  local ttitle = tab_title(tab)
+  local process_icon = get_process_icon(tab)
 
+  if has_unseen_output then
+    title = string.format(' %s %s  %s ', process_icon, wezterm.nerdfonts.fa_asterisk, ttitle)
+  else
+    title = string.format(' %s  %s ', process_icon, ttitle)
+  end
+
+  return wezterm.format({
+    { Attribute = { Intensity = 'Bold' } },
+    { Text = title }
+  })
+end)
 
 
 return config
